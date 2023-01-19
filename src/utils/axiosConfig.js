338d091f1b4e20d75eb33b04fd162b1511c244e7
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-01-19 23:11:37 +1100

    update backend API url romove port 5000

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://shoppingmart-backend.onrender.com:5000/api",
+  baseURL: "https://shoppingmart-backend.onrender.com/api",
 });
