718e2a00926ba38ca8c249b76b150feda263b1ac
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-17 23:13:12 +1100

    update contactus page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import "../Contactus.css";
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <div>
+      <form>
+        <label for="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+          required
+        />
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+          required
+        />
+        <label for="message">Message</label>
+        <input
+          type="message"
+          id="message"
+          name="message"
+          placeholder="Enter your message"
+          required
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactUs;
